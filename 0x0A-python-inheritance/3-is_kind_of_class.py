fcc9f8ecfddee6d67681e7d9d5c15a3303ae70cf
Paul Michael <paulebi4eva@gmail.com>
2023-04-19 07:43:42 -0700

    Remove module

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""
+===================================
+module with method is_kind_of_class
+===================================
+"""
+
+
 def is_kind_of_class(obj, a_class):
-    """
-        Return True if obj is an instance of a_class
-        or a subclass of a_class
-        Otherwise, return False.
-    """
+    """Method that return True if an object is an instance of a class
+    that inherited from"""
+
     return isinstance(obj, a_class)
